36b3d71cd8b7aa4e3a6df5b71af634e2f5382326
Will Baker <william+gitlab@polymathrobotics.com>
2023-03-10 08:38:41 -0800

    Use current dir

--- main.sh
+++ main.sh
@@ -1,2 +1,4 @@
-source "${GITAWAREPROMPT}/colors.sh"
-source "${GITAWAREPROMPT}/prompt.sh"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+source "${SCRIPT_DIR}/colors.sh"
+source "${SCRIPT_DIR}/prompt.sh"
